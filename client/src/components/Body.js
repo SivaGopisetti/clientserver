609255a4c9341938433134c01a0b810133d0b399
SivaGopisetti <gsknaidu1@gmail.com>
2021-11-08 13:28:19 +0530

    commit on 'master'

---
+++ client/src/components/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+    render() {
+        return (
+            <div>
+                <h2>BODY</h2>
+            </div>
+        );
+    }
+}
+
+export default Body;
